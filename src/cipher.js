d739db4de8a51a728b12c41f3dbcc0b2ea7e38f0
yuriquezada <yuri_quezada@hotmail.com>
2018-11-29 11:39:35 -0500

    Probando background image

--- src/cipher.js
+++ src/cipher.js
@@ -1,58 +1,53 @@
 window.cipher = {
-  encode: (offSet,string)=>{
-    let newAscii;
-    let asciiCode;
-    let textOutput = '';
+    encode: (offSet,string) => {
+        let asciiCode;
+        let newAscii;
+        let textOutput = '';
+        for (let i=0; i<string.length; i++) {
+            asciiCode=string.charCodeAt(i);
+            if (asciiCode>=65 && asciiCode<=90) {                  //para las mayúsculas
+                newAscii= ((asciiCode - 65 + offSet)%26+65);
+                textOutput+=String.fromCharCode(newAscii);
+            }
+            else if (asciiCode>=97 && asciiCode<=122) {          //para las minusculas
+                newAscii = ((asciiCode - 97 + offSet)%26+97);
+                textOutput+=String.fromCharCode(newAscii);
+            }
+            else {                                                  //Cualquier otro caracter
+                textOutput+=String.fromCharCode(asciiCode);
+            }
+        }
+        return textOutput;
+    },
 
-    for (let i=0; i < string.length; i++){
-      asciiCode=string.charCodeAt(i);
+    decode: (offSet,string) => {
+        let asciiCode;
+        let newAscii;
+        let textOutput = '';
+        for (let i=0; i<string.length; i++) {
+            asciiCode=string.charCodeAt(i);
+            if (asciiCode >=65 && asciiCode <=90) {      //para las mayúsculas
+                newAscii = (asciiCode - 90 - offSet) % 26 + 90;
+                textOutput+=String.fromCharCode(newAscii);
+            }
+            else if (asciiCode >=97 && asciiCode <=122) {     //para las minusculas
+                newAscii = (asciiCode - 122 - offSet) % 26 + 122;
+                textOutput+=String.fromCharCode(newAscii);
+            }
+            else {                                           //Cualquier otro caracter         
+                textOutput+=String.fromCharCode(asciiCode);
+            }
+        }
+        return textOutput;
+    },
 
-      if (asciiCode >=65 && asciiCode <=90){                  //para las mayusculas
-        newAscii= ((asciiCode - 65 + offSet)%26+65);
-        textOutput+=String.fromCharCode(newAscii);
-      }
-      else if (asciiCode >=97 && asciiCode <=122) {          //para las minusculas
-        newAscii = ((asciiCode - 97 + offSet)%26+97);
-        textOutput+=String.fromCharCode(newAscii);
-      }
-      else{                                                  //Cualquier otro caracter
-        textOutput+=String.fromCharCode(asciiCode);
-      }
+    createCipherWithOffset: (offSet) => {
+        let objectCipher = {
+            encode: (string) =>
+                cipher.encode(offSet,string),
+            decode: (string) =>
+                cipher.decode(offSet,string)
+        };
+        return objectCipher;
     }
-    return textOutput;
-  },
-
-  decode: (offSet,string)=>{
-    let textOutput = '';
-    let newAscii = 0;
-
-    for (let i=0; i < string.length; i++){
-    const asciiCode=string.charCodeAt(i);
-
-    if (asciiCode >=65 && asciiCode <=90){
-        newAscii = (asciiCode - 90 - offSet) % 26 + 90;
-        textOutput+=String.fromCharCode(newAscii);
-      }
-      else if (asciiCode >=97 && asciiCode <=122) {
-        newAscii = (asciiCode - 122 - offSet) % 26 + 122;
-        textOutput+=String.fromCharCode(newAscii);
-      }
-      else{
-        textOutput+=String.fromCharCode(asciiCode);
-      }
-    }
-    return textOutput;
-  },
-
-
-  createCipherWithOffset: (offSet)=> {
-      let objectCipher = {
-        encode: (string) =>
-          cipher.encode(offSet,string),
-        decode: (string) =>
-          cipher.decode(offSet,string)
-      };
-
-     return objectCipher;
-  }
 };
